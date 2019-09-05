c23a728b92539d48f9d92ccc68f889a64386799d
sherman.david.marshall@gmail.com <sherman.david.marshall@gmail.com>
2019-09-04 23:06:20 -0700

    Update PriorityQueue to use generics, and potentially be bounded

--- HW/priorityqueue/PriorityQueue.java
+++ HW/priorityqueue/PriorityQueue.java
@@ -1,79 +1,105 @@
-package priorityqueue;
+import java.util.*;
 
-import java.util.List;
-import java.util.ArrayList;
-
-public class PriorityQueue {
-	private List<Student> students = new ArrayList();
+public class PriorityQueue<PriorityQueueItem> {
+	private List<PriorityQueueItem> items;
 	private float maxPriority = 0.0f;
+	private float minPriority = Integer.MIN_VALUE;
+	private int maxSize = Integer.MAX_VALUE;
 
-	public PriorityQueue() {}
+	public PriorityQueue() {
+		items = new ArrayList();
+	}
+
+	public PriorityQueue(int size) {
+		items = new ArrayList(maxSize = size);
+	}
 
 	/**
-	 * method takes an input Student object and performs an
-	 * insertion sort to store it using a binary search
+	 * method takes an input PriorityQueueItem object and performs an insertion sort
+	 * to store it using a binary search
 	 */
-	public void add(Student student) {
-		int max = 0, min = students.size() - 1, position = min / 2;
-		float priority = student.getPriority();
+	public synchronized void add(PriorityQueueItem item) {
+		int max = 0, min = items.size() - 1, position = min / 2;
+		float priority = item.getPriority();
 
-		//Add student at front of queue if priority is greater than current maximum
-		if (priority > maxPriority) {
+		// Add student at front of queue if priority is greater than current maximum
+		if (priority > maxPriority && items.size() < maxSize) {
 			maxPriority = priority;
-			students.add(0, student);
+			items.add(0, item);
 			return;
 		}
 
-		//Add the student when the size is 1. The algorithm hasn't scaled yet
-		if (students.size() == 1) {
-			students.add(student);
+		// Add the item when the size is 1. The algorithm hasn'PriorityQueueItem scaled
+		// yet
+		if (items.size() == 1) {
+			items.add(item);
 			return;
 		}
 
-		Student ref = null;
+		PriorityQueueItem ref = null;
 		boolean insertAfter = false;
 		do {
-			ref = students.get(position);
-		
-			//Add in upper half of data
+			ref = items.get(position);
+
+			// Add in upper half of data
 			if (priority < ref.getPriority()) {
 				max = position;
 				int val = (int) Math.ceil((double) (min - max) / 2.0);
 				val = val == 0 ? val++ : val;
 				position = max + val;
-				insertAfter = position >= students.size() ? true : false;
+				insertAfter = position >= items.size() ? true : false;
 
-			} else { //Add in lower half
+			} else { // Add in lower half
 				min = position;
 				int val = (int) Math.ceil((double) (min - max) / 2.0);
 				val = val == 0 ? val++ : val;
 				position = min - val;
 			}
 		} while ((min - max) > 1);
-	
+
 		if (insertAfter) {
-			students.add(student);
+			items.add(item);
 		} else {
-			//If position located is greater, add after
-			if (students.get(position).getPriority() > priority) {
-				students.add(position + 1, student);
-			} else {
-				students.add(position, student);
+			position += (items.get(position).getPriority() > priority) ? 1 : 0;
+			if (items.size() == maxSize && position != maxSize) {
+				items.remove(maxSize-1);
+				items.add(position, item);
 			}
 		}
 	}
 
-	public Student remove() {
-		return students.size() > 0 ? students.remove(0) : null;
+	public int getMaxSize() {
+		return maxSize;
 	}
 
-	public List<Student> getStudents() {
-		return students;
+	public synchronized PriorityQueueItem remove() {
+		return items.size() > 0 ? items.remove(0) : null;
 	}
 
-	public void output() {
-		for (Student s : students) {
-			System.out.println(s.toString());
+	public synchronized List<PriorityQueueItem> getItems() {
+		return items;
+	}
+
+	public static interface PriorityQueueItem {
+		public float getPriority();
+	}
+
+	public static void main(String ...args) {
+		PriorityQueue<PriorityQueueItem> queue = new PriorityQueue(128);
+		
+		for (int x = 0; x < 1000; x++) {
+			float i = (float) (Math.random() * 1000000);
+			PriorityQueueItem pqi  = new PriorityQueueItem() {
+				public float getPriority() {
+					return (float) i;
+				}
+			};
+			queue.add(pqi);
+		}
+		int counter = 0;
+		for (PriorityQueueItem pqi : queue.getItems()) {
+			System.out.println(counter++ + " : " + pqi.getPriority());
 		}
 	}
 }
+
